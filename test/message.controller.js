b1435dad62f5669d6b3c7f5c8f1aef2eab6d2364
Matt Mueller <mattmuelle@gmail.com>
2012-04-29 00:25:44 -0700

    added message controller test

--- test/message.controller.js
+++ test/message.controller.js
@@ -10,14 +10,44 @@ function encodeBasicAuth(user, pass) {
   return 'Basic ' + new Buffer(user + ':' + pass).toString('base64');
 }
 
+var groups = [
+  { id : 123456, name : 'Javascript'},
+  { id : 654321, name : 'Soccer'}
+];
+
+var user = {
+  name : 'Matt',
+  email : 'mattmuelle@gmail.com',
+  password : 'test',
+  groups : groups
+};
+
 describe('User Controller', function() {
-  var user;
 
   // Run before starting the suite
   before(function(done) {
     if(client.connected) return done();
     client.on('ready', done);
+
+    groups = new Groups(groups),
+    user = new User(user);
+
+    groups.save(function(err, collection) {
+      if(err) return done(err);
+      user.save(function(err, model) {
+        if(err) return done(err);
+        return done();
+      });
+    });
   });
 
+  describe('POST /messages', function() {
+
+    it('should create a new message', function(done) {
+
+      console.log(user);
+
+    });
+  });
 
 });
