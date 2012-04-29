06e9eae4782a2e395b23e6a4beaf4d3cb0e3015d
Matt Mueller <mattmuelle@gmail.com>
2012-04-28 17:41:17 -0700

    creating users with unit tests!

--- test/user.controller.js
+++ test/user.controller.js
@@ -1,5 +1,6 @@
 var expect = require('expect.js'),
     request = require('./support/request'),
+    client = require('../support/client'),
     app = require('../app.js');
 
 function encodeBasicAuth(user, pass) {
@@ -8,17 +9,22 @@ function encodeBasicAuth(user, pass) {
 
 describe('User Controller', function() {
 
-
+  // Run before starting the suite
+  before(function(done) {
+    if(client.connected) return done();
+    client.on('ready', done);
+  });
 
   describe('POST /users', function() {
 
     it('should create a new user', function(done) {
       
       request(app)
-        .get('/groups')
-        .set('Authorization', encodeBasicAuth('matt', 'test'))
+        .post('/users')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .write('name=Matt Mueller&email=mattmuelle@gmail.com&password=test')
         .end(function(res) {
-          console.log(res);
+          console.log(res.body);
           done();
         });
 
@@ -27,6 +33,15 @@ describe('User Controller', function() {
   }); 
 
 
+  // Flush the database after each test
+  after(function(done) {
+    client.flushdb(function(err) {
+      if(err) return done(err);
+      done();
+    })
+  });
+
 });
 
 
+//.set('Authorization', encodeBasicAuth('mattmuelle@gmail.com', 'test'))
